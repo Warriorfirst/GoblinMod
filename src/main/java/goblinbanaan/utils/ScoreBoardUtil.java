3644a58e83c5a1893b1a43e636a757de4d11f8c8
Kevin Nguyen <kevin.nguyen@hva.nl>
2023-12-20 14:08:01 +0100

    Removed some old (unused) code

--- src/main/java/goblinbanaan/utils/ScoreBoardUtil.java
+++ src/main/java/goblinbanaan/utils/ScoreBoardUtil.java
@@ -3,19 +3,29 @@ package goblinbanaan.utils;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiPlayerTabOverlay;
 import net.minecraft.scoreboard.Score;
 import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.client.network.NetworkPlayerInfo;
 
+import java.lang.reflect.Field;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
+
 
 public class ScoreBoardUtil {
 
     /**
      * Data from Skyhanni
      * Source: https://github.com/hannibal002/SkyHanni
+     *
      * @author CalMWolfs
      */
     private static final List<String> hypixelsWeirdIcons = Arrays.asList(
@@ -23,25 +33,28 @@ public class ScoreBoardUtil {
             "\uD83D\uDC7E", "\uD83C\uDF20", "\uD83C\uDF6D", "⚽", "\uD83C\uDFC0", "\uD83D\uDC79",
             "\uD83C\uDF81", "\uD83C\uDF89", "\uD83C\uDF82", "\uD83D\uDD2B"
     );
-    public static String fixFormatting(String input,Boolean cleanColor) {
+
+    public static String fixFormatting(String input, Boolean cleanColor) {
         for (String weirdIcon : hypixelsWeirdIcons) {
             if (input.contains(weirdIcon)) {
                 String[] parts = input.split(weirdIcon, 2);
-                input = parts[0] + (parts.length > 1 ? parts[1].length()>2?parts[1].substring(2):"" : "");
+                input = parts[0] + (parts.length > 1 ? parts[1].length() > 2 ? parts[1].substring(2) : "" : "");
             }
         }
-        if(cleanColor) input = Utils.cleanColor(input);
+        if (cleanColor) input = Utils.cleanColor(input);
         return input;
     }
 
     /**
      * Taken from Danker's Skyblock Mod and modified under GPL 3.0 license
      * https://github.com/bowser0000/SkyblockMod/blob/master/LICENSE
+     *
      * @author bowser0000
      */
     public static List<String> getSidebarLines() {
         return getSidebarLines(true);
     }
+
     public static List<String> getSidebarLines(boolean clean) {
         List<String> lines = new ArrayList<>();
         if (Minecraft.getMinecraft().theWorld == null) return lines;
@@ -66,7 +79,7 @@ public class ScoreBoardUtil {
 
         for (Score score : scores) {
             ScorePlayerTeam team = scoreboard.getPlayersTeam(score.getPlayerName());
-            lines.add(fixFormatting(ScorePlayerTeam.formatPlayerName(team, score.getPlayerName()),clean));
+            lines.add(fixFormatting(ScorePlayerTeam.formatPlayerName(team, score.getPlayerName()), clean));
         }
         // Reverse it so its from top to bottom
         lines.add(objective.getDisplayName());
@@ -74,3 +87,4 @@ public class ScoreBoardUtil {
         return lines;
     }
 }
+
