4f979ff5fc28b0815049197214dd02fcba404c26
Warriorfirst <84595526+Warriorfirst@users.noreply.github.com>
2024-01-13 23:22:59 +0100

    pre4 helpers

---
+++ src/main/java/goblinbanaan/features/dungeons/BonzoTimer.java
@@ -0,0 +1,25 @@
+package goblinbanaan.features.dungeons;
+
+import goblinbanaan.config.GoblinConfig;
+import goblinbanaan.utils.Utils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import static goblinbanaan.utils.Utils.drawTitle;
+
+
+public class BonzoTimer{
+
+    @SubscribeEvent
+    public void onChat(ClientChatReceivedEvent event) {
+        String message = event.message.getUnformattedText();
+
+        if(GoblinConfig.bonzoTimer && message.contains("Your ⚚ Bonzo's Mask saved your life!") || message.contains("Your Bonzo's Mask saved your life!")) {
+            drawTitle("", null, 20);
+            Utils.setTimeout(() -> Minecraft.getMinecraft().ingameGUI.displayTitle("Rod now!", null, 0, 20, 0), 2500);
+        }
+
+    }
+}
